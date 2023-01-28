1e2f7cf8c26977e7016e87c66f95ad6b66a23d78
TDR <tdr.minecraft@gmail.com>
2023-01-28 19:14:09 +0100

    add models back in

---
+++ src/main/java/de/bentzin/hangar/api/development/ProjectSearchTest.java
@@ -0,0 +1,45 @@
+package de.bentzin.hangar.api.development;
+
+import de.bentzin.hangar.api.ApiClient;
+import de.bentzin.hangar.api.ApiException;
+import de.bentzin.hangar.api.client.ProjectsApi;
+import org.jetbrains.annotations.NotNull;
+import org.openapitools.client.model.PaginatedResultProject;
+import org.openapitools.client.model.Project;
+import org.openapitools.client.model.RequestPagination;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @author Ture Bentzin
+ * 28.01.2023
+ */
+public class ProjectSearchTest {
+
+    public static void main(String[] args) throws IOException, ApiException {
+        InputStreamReader i = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(i);
+        ApiClient client = new ApiClient();
+        ProjectsApi projectsApi = new ProjectsApi(client);
+        while (true) {
+            System.out.print("Type query: ");
+            String input = reader.readLine();
+            System.out.println();
+            System.out.println("-> " + input);
+            System.out.println("searching...");
+            PaginatedResultProject search = search(projectsApi, input);
+            System.out.println("------------------------------------");
+            System.out.println("Seitennation: " + search.getPagination());
+            for (Project project : search.getResult()) {
+                System.out.println(project.getName() + " by " + project.getNamespace().getOwner() + "[" + project.getCategory().name() + "]");
+            }
+            System.out.println(">----------------------------------<");
+        }
+    }
+
+    private static PaginatedResultProject search(@NotNull ProjectsApi api, String name) throws ApiException {
+        return api.getProjects(new RequestPagination().offset(3L), true, null, null, null, null, name, null, null);
+    }
+}
--- src/main/java/de/bentzin/hangar/api/development/Test.java
+++ src/main/java/de/bentzin/hangar/api/development/Test.java
@@ -1,17 +1,13 @@
 package de.bentzin.hangar.api.development;
 
-import de.bentzin.hangar.api.ApiException;
-import de.bentzin.hangar.api.HangarJavaAPI;
-import de.bentzin.hangar.api.base.HangarAPIVersion;
-import de.bentzin.hangar.api.base.HangarInstance;
-import de.bentzin.hangar.api.client.UsersApi;
-import de.bentzin.hangar.api.service.HangarAPIService;
-import okhttp3.ResponseBody;
-import org.openapitools.client.model.PaginatedResultUser;
-import org.openapitools.client.model.RequestPagination;
-import org.openapitools.client.model.User;
+import de.bentzin.hangar.api.*;
+import de.bentzin.hangar.api.client.ProjectsApi;
+import org.jetbrains.annotations.NotNull;
+import org.openapitools.client.model.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Ture Bentzin
@@ -20,20 +16,101 @@ import java.io.IOException;
 public class Test {
 
     public static void main(String[] args) throws IOException {
-        HangarJavaAPI hangarJavaAPI = new HangarJavaAPI(Keyholder.testHolder(), HangarInstance.stagingInstance, HangarAPIVersion.v1);
-        HangarAPIService service = hangarJavaAPI.getService();
-        ResponseBody authors = service.getAuthors().execute().body();
 
         try {
-            
-            PaginatedResultUser name = new UsersApi().getAuthors(new RequestPagination(), "name");
-            for (User user : name.getResult()) {
-                System.out.println(user.getName());
-            }
+            List<Project> allProjects = getAllProjects();
+            allProjects.stream().map(Project::getName).forEach(System.out::println);
+
         } catch (ApiException e) {
             throw new RuntimeException(e);
         }
 
-        System.out.println(authors);
+        /*
+        try {
+
+            ApiClient apiClient = new ApiClient();
+            Configuration.setDefaultApiClient(apiClient);
+
+            AuthenticationApi authenticationApi = new AuthenticationApi();
+            Response execute = authenticationApi.authenticateCall("278a0ce4-9dbd-4648-9281-820b2a0cec03", new ApiCallback() {
+                @Override
+                public void onFailure(ApiException e, int statusCode, Map responseHeaders) {
+                    System.out.println("e = " + e + ", statusCode = " + statusCode + ", responseHeaders = " + responseHeaders);
+                }
+
+                @Override
+                public void onSuccess(Object result, int statusCode, Map responseHeaders) {
+                    System.out.println("result = " + result + ", statusCode = " + statusCode + ", responseHeaders = " + responseHeaders);
+                }
+
+                @Override
+                public void onUploadProgress(long bytesWritten, long contentLength, boolean done) {
+                    System.out.println("bytesWritten = " + bytesWritten + ", contentLength = " + contentLength + ", done = " + done);
+                }
+
+                @Override
+                public void onDownloadProgress(long bytesRead, long contentLength, boolean done) {
+                    System.out.println("bytesRead = " + bytesRead + ", contentLength = " + contentLength + ", done = " + done);
+                }
+            }).execute();
+
+            User tureBentzin = new UsersApi().getUser("TureBentzin");
+
+            PaginatedResultProject projects =
+                    new ProjectsApi().getProjects(new RequestPagination().limit(999999L), null, null, null, null, null,
+                            null, null, null);
+            System.out.println(projects.getPagination());
+            for (Project project : projects.getResult()) {
+                System.out.println(project.getName());
+            }
+            System.out.println("bentzin:" + tureBentzin);
+            System.out.println("response" + execute);
+
+            /*
+            ApiSession authenticate = new AuthenticationApi().authenticate("7c7c6b56-46a2-461b-a560-509ad75931b3");
+            PaginatedResultUser name = new UsersApi().getAuthors(new RequestPagination(), "name");
+            for (User user : name.getResult()) {
+                System.out.println(user.getName());
+            }
+
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+  */
+    }
+
+    public static @NotNull List<Project> getAllProjects() throws ApiException {
+        ArrayList<Project> projectArrayList = new ArrayList<>();
+        long current = 0;
+        long count = 0;
+            PaginatedResultProject initialResult = getNextProjectsResult(0);
+            count = initialResult.getPagination().getCount();
+            current = initialResult.getPagination().getOffset();
+            projectArrayList.addAll(initialResult.getResult());
+        System.out.println("initial: " + current + "/" + count + " [" + projectArrayList.size() + "]");
+            while (current < count) {
+                PaginatedResultProject nextProjectsResult = getNextProjectsResult(current);
+                count = nextProjectsResult.getPagination().getCount();
+                current = nextProjectsResult.getPagination().getOffset();
+                projectArrayList.addAll(nextProjectsResult.getResult());
+                System.out.println("loading: " + current + "/" + count + " [" + projectArrayList.size() + "]");
+                if(projectArrayList.size() > count)  {
+                    System.err.println("PANIC!");
+                    break;
+                }
+            }
+        return projectArrayList;
+    }
+
+    public static PaginatedResultProject getNextProjectsResult(long offset) throws ApiException { {
+        RequestPagination requestPagination = new RequestPagination().offset(offset);
+        return new ProjectsApi().getProjects(requestPagination, true, null, null, null, null,
+                null, null, null);
+    }
+
+
+
     }
 }
